d17e768b7722b55c8486cc92ea843e1bcea9f79f
Oleksii Vasyliuk <olvas@smile.fr>
2019-06-05 16:45:17 +0300

    add post creating functionality

--- src/components/create.component.js
+++ src/components/create.component.js
@@ -1,6 +1,7 @@
 import {Component} from "../core/component";
 import {Form} from '../core/form';
 import {Validators} from "../core/validators";
+import {apiService} from "../service/api.service";
 
 export class CreateComponent extends Component {
   constructor(id) {
@@ -9,25 +10,24 @@ export class CreateComponent extends Component {
 
   init() {
     this.$el.addEventListener('submit', submitHandler.bind(this));
-    this.form = new Form(this.$el, {
+     this.form = new Form(this.$el, {
       title: [Validators.required],
-      fulltext: [Validators.required]
+      fulltext: [Validators.required, Validators.minLength(10)]
     });
   }
 }
 
-function submitHandler(event) {
+async function submitHandler(event) {
   event.preventDefault();
 
   if(this.form.isValid()){
     const formData = {
       type: this.$el.type.value,
+      date: new Date().toLocaleDateString(),
       ...this.form.value()
     };
-    console.log('Submit', formData, this.form.value());
+    await apiService.createPost(formData);
+    this.form.clear();
+    alert('Запись создана в БД');
   }
-
-
-
-
 }
---
+++ src/service/api.service.js
@@ -0,0 +1,21 @@
+class ApiService {
+  constructor(baseUrl) {
+    this.url = baseUrl;
+  }
+
+  async createPost(post) {
+    try {
+      const request = new Request(this.url + '/posts.json', {
+        method: 'post',
+        body: JSON.stringify(post)
+      });
+      const responce = await fetch(request);
+      return await responce.json()
+    } catch (error) {
+      console.error(error)
+    }
+
+  }
+}
+
+export const apiService = new ApiService('https://js-blog-11064.firebaseio.com');
